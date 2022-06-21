e3b30b280e4eabda8b4dc690703c348756ea07c5
Tez <tez.park@sendbird.com>
2022-06-21 19:19:41 +0900

    * Modified some view to be able to change its date format   * Opened `SBUMessageDateView`   * Opened `SBUChannelCell`   * Added `SBUMessageDateView.dateFormat`   * Added `SBUOpenChannelContentBaseMessageCell.dateFormat`   * Added `SBUMessageSearchResultCell.dateFormat`   * Added parameters to `SBUChannelCell buildLastUpdatedDate()`: `dateFormat`, `timeFormat` * Added public classes and interfaces regarding message cells   * Added open/public interfaces in message cells.   * Added `SBULinkClickableTextView`   * Added `SBUMessageWebView` , `SBUMessageWebViewModel` and `SBUOpenChannelMessageWebView`   * Added `SBUUserMessageTextViewDelegate` , `SBUUserMessageTextViewModel` and `SBUUserMessageTextView`   * Added `SBUUserNameView`   * Added `SBUFileViewerDelegate` and `SBUFileViewer`   * Added `QuotedFileCommonContentView` and `QuotedFileImageContentView`   * Added `SBUMessageReactionView` * Added `SBUDateFormatSet` * Added `Date.sbu_toString(dateFormat:localizedFormat)` * Changed `Date.lastUpdatedTime` function in `Data+SBUIKit` access level to public * Fixed not called completion handler on unregister pushToken

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendbirdUIKit",
-            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.0.0-beta.3/SendbirdUIKit.xcframework.zip",
-            checksum: "c31ce9fec29c3f56eb545a93ef896b3333449f22accfa7903917e920745dcc34"
+            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.0.0-beta.4/SendbirdUIKit.xcframework.zip",
+            checksum: "c0ba2c370795574025ccc9859cfcae0fca802cfacce12999f75c16aff9bb6b73"
         ),
         .target(
             name: "SendbirdUIKitTarget",
