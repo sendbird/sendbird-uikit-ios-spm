bb85dccfa3fe23bae74bcc4312423df0dd673036
Jaesung Lee <jaesung.lee@sendbird.com>
2022-07-12 14:07:23 +0900

    * UIKit v3.0.0 officially version     * Support `modules` and `components` in the UIKit     * See more details and breaking changes. [[details](/changelogs/BREAKING_CHANGES_V3.md)]     * See the Migration Guide for Converting V2 to V3. [[details](/changelogs/MIGRATION_GUIDE_V3.md)]

--- Package.swift
+++ Package.swift
@@ -14,22 +14,22 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            name: "SendBirdSDK",
-            url: "https://github.com/sendbird/sendbird-chat-ios-spm",
-            from: "3.1.13"
+            name: "SendbirdChatSDK",
+            url: "https://github.com/sendbird/sendbird-chat-sdk-ios",
+            from: "4.0.5"
         ),
     ],
     targets: [
         .binaryTarget(
             name: "SendbirdUIKit",
-            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.0.0-beta.4/SendbirdUIKit.xcframework.zip",
-            checksum: "c0ba2c370795574025ccc9859cfcae0fca802cfacce12999f75c16aff9bb6b73"
+            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.0.0/SendbirdUIKit.xcframework.zip",
+            checksum: "4edf5386518dcdc259aef1cfae52940c833cc67971c780df34d675d4f6534ea8"
         ),
         .target(
             name: "SendbirdUIKitTarget",
             dependencies: [
                 .target(name: "SendbirdUIKit"),
-                .product(name: "SendBirdSDK", package: "SendBirdSDK")
+                .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK")
             ],
             path: "Sources"
         ),
