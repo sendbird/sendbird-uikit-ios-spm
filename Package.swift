05313a8c3571b34d355c6d6dc973bcb17b3ccff7
Jaesung Lee <jaesung.lee@sendbird.com>
2022-08-17 17:35:12 +0900

    * Improved stability

--- Package.swift
+++ Package.swift
@@ -22,8 +22,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SendbirdUIKit",
-            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.1.0/SendbirdUIKit.xcframework.zip",
-            checksum: "d9a9f06835a04984b2253eac5f828ed76c34c351502086b36e564ee794a14fa9"
+            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/v3.1.1/SendbirdUIKit.xcframework.zip",
+            checksum: "822409d7e784e1b04ab07feefb0137d13659e276b7fe1175da04f0fd39972514"
         ),
         .target(
             name: "SendbirdUIKitTarget",
