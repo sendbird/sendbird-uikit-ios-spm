a5fa6f727b60f586351f81853af64b16c603dd47
Jed Gyeong <jed@sendbird.com>
2024-12-06 15:10:21 +0900

    Added template module config to package setting file.

--- Package.swift
+++ Package.swift
@@ -11,6 +11,10 @@ let package = Package(
             name: "SendbirdUIKit",
             targets: ["SendbirdUIKitTarget"]
         ),
+        .library(
+            name: "SendbirdUIMessageTemplate",
+            targets: ["SendbirdUIMessageTemplateTarget"]
+        ),
     ],
     dependencies: [
         .package(
@@ -25,6 +29,11 @@ let package = Package(
             url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/3.28.1/SendbirdUIKit.xcframework.zip", // SendbirdUIKit_URL
             checksum: "505f30d55e1a2d2ab270c7d0ee4542da359fa58c3ad07190c5f631d02e0d3143" // SendbirdUIKit_CHECKSUM
         ),
+        .binaryTarget(
+            name: "SendbirdUIMessageTemplate",
+            url: "https://github.com/sendbird/sendbird-uikit-ios/releases/download/3.28.1/SendbirdUIMessageTemplate.xcframework.zip", // SendbirdUIMessageTemplate_URL
+            checksum: "505f30d55e1a2d2ab270c7d0ee4542da359fa58c3ad07190c5f631d02e0d3143" // SendbirdUIMessageTemplate_CHECKSUM
+        ),
         .target(
             name: "SendbirdUIKitTarget",
             dependencies: [
@@ -32,6 +41,15 @@ let package = Package(
                 .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK")
             ],
             path: "Framework/Dependency",
+            exclude: ["../../Sources"]
+        ),
+        .target(
+            name: "SendbirdUIKitTarget",
+            dependencies: [
+                .target(name: "SendbirdUIKit"),
+                .product(name: "SendbirdChatSDK", package: "SendbirdChatSDK")
+            ],
+            path: "Framework/Module/MessageTemplate",
             exclude: ["../../Sources"]
         ),
     ]
